65344dd28de1d25ac4d202d8030a992c2b208fe4
Sophie MacRaild <macra08842@students.pvcc.vic.edu.au>
2022-03-04 15:25:02 +1100

    final push for SAC 1A

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,43 +22,56 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.tbxName = New System.Windows.Forms.TextBox()
+        Me.tbxEnglish = New System.Windows.Forms.TextBox()
+        Me.tbxMaths = New System.Windows.Forms.TextBox()
         Me.lblName = New System.Windows.Forms.Label()
         Me.lblEnglish = New System.Windows.Forms.Label()
         Me.lblMaths = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
         Me.lblPE = New System.Windows.Forms.Label()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbxPE = New System.Windows.Forms.TextBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblClass = New System.Windows.Forms.Label()
+        Me.lblMarks = New System.Windows.Forms.Label()
+        Me.btnNameEnter = New System.Windows.Forms.Button()
+        Me.btnEnglishEnter = New System.Windows.Forms.Button()
+        Me.btnMathsEnter = New System.Windows.Forms.Button()
+        Me.btnPeEnter = New System.Windows.Forms.Button()
+        Me.lblAllNames = New System.Windows.Forms.Label()
+        Me.lblEnglishMarks = New System.Windows.Forms.Label()
+        Me.lblMathsMarks = New System.Windows.Forms.Label()
+        Me.lblPEMarks = New System.Windows.Forms.Label()
+        Me.lblEngNum = New System.Windows.Forms.Label()
+        Me.lblMathNum = New System.Windows.Forms.Label()
+        Me.lblPENum = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'tbxName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(387, 104)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.tbxName.Location = New System.Drawing.Point(234, 94)
+        Me.tbxName.Name = "tbxName"
+        Me.tbxName.Size = New System.Drawing.Size(100, 20)
+        Me.tbxName.TabIndex = 0
         '
-        'TextBox2
+        'tbxEnglish
         '
-        Me.TextBox2.Location = New System.Drawing.Point(387, 164)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 1
+        Me.tbxEnglish.Location = New System.Drawing.Point(234, 161)
+        Me.tbxEnglish.Name = "tbxEnglish"
+        Me.tbxEnglish.Size = New System.Drawing.Size(100, 20)
+        Me.tbxEnglish.TabIndex = 1
         '
-        'TextBox3
+        'tbxMaths
         '
-        Me.TextBox3.Location = New System.Drawing.Point(387, 240)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 2
+        Me.tbxMaths.Location = New System.Drawing.Point(234, 221)
+        Me.tbxMaths.Name = "tbxMaths"
+        Me.tbxMaths.Size = New System.Drawing.Size(100, 20)
+        Me.tbxMaths.TabIndex = 2
         '
         'lblName
         '
         Me.lblName.AutoSize = True
-        Me.lblName.Location = New System.Drawing.Point(265, 107)
+        Me.lblName.Location = New System.Drawing.Point(112, 93)
         Me.lblName.Name = "lblName"
         Me.lblName.Size = New System.Drawing.Size(82, 13)
         Me.lblName.TabIndex = 3
@@ -67,7 +80,7 @@ Partial Class Form1
         'lblEnglish
         '
         Me.lblEnglish.AutoSize = True
-        Me.lblEnglish.Location = New System.Drawing.Point(265, 171)
+        Me.lblEnglish.Location = New System.Drawing.Point(112, 164)
         Me.lblEnglish.Name = "lblEnglish"
         Me.lblEnglish.Size = New System.Drawing.Size(73, 13)
         Me.lblEnglish.TabIndex = 4
@@ -76,61 +89,191 @@ Partial Class Form1
         'lblMaths
         '
         Me.lblMaths.AutoSize = True
-        Me.lblMaths.Location = New System.Drawing.Point(265, 247)
+        Me.lblMaths.Location = New System.Drawing.Point(117, 228)
         Me.lblMaths.Name = "lblMaths"
         Me.lblMaths.Size = New System.Drawing.Size(68, 13)
         Me.lblMaths.TabIndex = 5
         Me.lblMaths.Text = "Maths Marks"
         '
-        'Button1
+        'btnEnter
         '
-        Me.Button1.Location = New System.Drawing.Point(571, 100)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnEnter.Location = New System.Drawing.Point(37, 338)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(108, 41)
+        Me.btnEnter.TabIndex = 6
+        Me.btnEnter.Text = "Enter Student data"
+        Me.btnEnter.UseVisualStyleBackColor = True
         '
         'lblPE
         '
         Me.lblPE.AutoSize = True
-        Me.lblPE.Location = New System.Drawing.Point(268, 313)
+        Me.lblPE.Location = New System.Drawing.Point(117, 292)
         Me.lblPE.Name = "lblPE"
         Me.lblPE.Size = New System.Drawing.Size(53, 13)
         Me.lblPE.TabIndex = 7
         Me.lblPE.Text = "PE Marks"
         '
-        'TextBox4
+        'tbxPE
         '
-        Me.TextBox4.Location = New System.Drawing.Point(387, 305)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox4.TabIndex = 8
+        Me.tbxPE.Location = New System.Drawing.Point(234, 290)
+        Me.tbxPE.Name = "tbxPE"
+        Me.tbxPE.Size = New System.Drawing.Size(100, 20)
+        Me.tbxPE.TabIndex = 8
         '
-        'Label1
+        'lblTitle
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(362, 42)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 9
-        Me.Label1.Text = "Label1"
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Location = New System.Drawing.Point(347, 41)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(101, 13)
+        Me.lblTitle.TabIndex = 9
+        Me.lblTitle.Text = "End of Year Results"
+        '
+        'lblClass
+        '
+        Me.lblClass.AutoSize = True
+        Me.lblClass.Location = New System.Drawing.Point(48, 406)
+        Me.lblClass.Name = "lblClass"
+        Me.lblClass.Size = New System.Drawing.Size(78, 13)
+        Me.lblClass.TabIndex = 10
+        Me.lblClass.Text = "Class numbers:"
+        '
+        'lblMarks
+        '
+        Me.lblMarks.AutoSize = True
+        Me.lblMarks.Location = New System.Drawing.Point(177, 352)
+        Me.lblMarks.Name = "lblMarks"
+        Me.lblMarks.Size = New System.Drawing.Size(78, 13)
+        Me.lblMarks.TabIndex = 11
+        Me.lblMarks.Text = "Student marks:"
+        '
+        'btnNameEnter
+        '
+        Me.btnNameEnter.Location = New System.Drawing.Point(350, 92)
+        Me.btnNameEnter.Name = "btnNameEnter"
+        Me.btnNameEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnNameEnter.TabIndex = 12
+        Me.btnNameEnter.Text = "Enter"
+        Me.btnNameEnter.UseVisualStyleBackColor = True
+        '
+        'btnEnglishEnter
+        '
+        Me.btnEnglishEnter.Location = New System.Drawing.Point(350, 161)
+        Me.btnEnglishEnter.Name = "btnEnglishEnter"
+        Me.btnEnglishEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnglishEnter.TabIndex = 13
+        Me.btnEnglishEnter.Text = "Enter"
+        Me.btnEnglishEnter.UseVisualStyleBackColor = True
+        '
+        'btnMathsEnter
+        '
+        Me.btnMathsEnter.Location = New System.Drawing.Point(350, 219)
+        Me.btnMathsEnter.Name = "btnMathsEnter"
+        Me.btnMathsEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnMathsEnter.TabIndex = 14
+        Me.btnMathsEnter.Text = "Enter"
+        Me.btnMathsEnter.UseVisualStyleBackColor = True
+        '
+        'btnPeEnter
+        '
+        Me.btnPeEnter.Location = New System.Drawing.Point(350, 287)
+        Me.btnPeEnter.Name = "btnPeEnter"
+        Me.btnPeEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnPeEnter.TabIndex = 15
+        Me.btnPeEnter.Text = "Enter"
+        Me.btnPeEnter.UseVisualStyleBackColor = True
+        '
+        'lblAllNames
+        '
+        Me.lblAllNames.AutoSize = True
+        Me.lblAllNames.Location = New System.Drawing.Point(443, 94)
+        Me.lblAllNames.Name = "lblAllNames"
+        Me.lblAllNames.Size = New System.Drawing.Size(97, 13)
+        Me.lblAllNames.TabIndex = 16
+        Me.lblAllNames.Text = "Names of Students"
+        '
+        'lblEnglishMarks
+        '
+        Me.lblEnglishMarks.AutoSize = True
+        Me.lblEnglishMarks.Location = New System.Drawing.Point(443, 161)
+        Me.lblEnglishMarks.Name = "lblEnglishMarks"
+        Me.lblEnglishMarks.Size = New System.Drawing.Size(86, 13)
+        Me.lblEnglishMarks.TabIndex = 17
+        Me.lblEnglishMarks.Text = "All English marks"
+        '
+        'lblMathsMarks
+        '
+        Me.lblMathsMarks.AutoSize = True
+        Me.lblMathsMarks.Location = New System.Drawing.Point(443, 221)
+        Me.lblMathsMarks.Name = "lblMathsMarks"
+        Me.lblMathsMarks.Size = New System.Drawing.Size(82, 13)
+        Me.lblMathsMarks.TabIndex = 18
+        Me.lblMathsMarks.Text = "All Maths Marks"
+        '
+        'lblPEMarks
+        '
+        Me.lblPEMarks.AutoSize = True
+        Me.lblPEMarks.Location = New System.Drawing.Point(443, 287)
+        Me.lblPEMarks.Name = "lblPEMarks"
+        Me.lblPEMarks.Size = New System.Drawing.Size(67, 13)
+        Me.lblPEMarks.TabIndex = 19
+        Me.lblPEMarks.Text = "All PE Marks"
+        '
+        'lblEngNum
+        '
+        Me.lblEngNum.AutoSize = True
+        Me.lblEngNum.Location = New System.Drawing.Point(640, 161)
+        Me.lblEngNum.Name = "lblEngNum"
+        Me.lblEngNum.Size = New System.Drawing.Size(114, 13)
+        Me.lblEngNum.TabIndex = 21
+        Me.lblEngNum.Text = "English Class Numbers"
+        '
+        'lblMathNum
+        '
+        Me.lblMathNum.AutoSize = True
+        Me.lblMathNum.Location = New System.Drawing.Point(640, 219)
+        Me.lblMathNum.Name = "lblMathNum"
+        Me.lblMathNum.Size = New System.Drawing.Size(104, 13)
+        Me.lblMathNum.TabIndex = 22
+        Me.lblMathNum.Text = "Math Class Numbers"
+        '
+        'lblPENum
+        '
+        Me.lblPENum.AutoSize = True
+        Me.lblPENum.Location = New System.Drawing.Point(640, 287)
+        Me.lblPENum.Name = "lblPENum"
+        Me.lblPENum.Size = New System.Drawing.Size(94, 13)
+        Me.lblPENum.TabIndex = 23
+        Me.lblPENum.Text = "PE class numbers:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.lblPENum)
+        Me.Controls.Add(Me.lblMathNum)
+        Me.Controls.Add(Me.lblEngNum)
+        Me.Controls.Add(Me.lblPEMarks)
+        Me.Controls.Add(Me.lblMathsMarks)
+        Me.Controls.Add(Me.lblEnglishMarks)
+        Me.Controls.Add(Me.lblAllNames)
+        Me.Controls.Add(Me.btnPeEnter)
+        Me.Controls.Add(Me.btnMathsEnter)
+        Me.Controls.Add(Me.btnEnglishEnter)
+        Me.Controls.Add(Me.btnNameEnter)
+        Me.Controls.Add(Me.lblMarks)
+        Me.Controls.Add(Me.lblClass)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.tbxPE)
         Me.Controls.Add(Me.lblPE)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnEnter)
         Me.Controls.Add(Me.lblMaths)
         Me.Controls.Add(Me.lblEnglish)
         Me.Controls.Add(Me.lblName)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.tbxMaths)
+        Me.Controls.Add(Me.tbxEnglish)
+        Me.Controls.Add(Me.tbxName)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -138,14 +281,27 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents tbxName As TextBox
+    Friend WithEvents tbxEnglish As TextBox
+    Friend WithEvents tbxMaths As TextBox
     Friend WithEvents lblName As Label
     Friend WithEvents lblEnglish As Label
     Friend WithEvents lblMaths As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnEnter As Button
     Friend WithEvents lblPE As Label
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents tbxPE As TextBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblClass As Label
+    Friend WithEvents lblMarks As Label
+    Friend WithEvents btnNameEnter As Button
+    Friend WithEvents btnEnglishEnter As Button
+    Friend WithEvents btnMathsEnter As Button
+    Friend WithEvents btnPeEnter As Button
+    Friend WithEvents lblAllNames As Label
+    Friend WithEvents lblEnglishMarks As Label
+    Friend WithEvents lblMathsMarks As Label
+    Friend WithEvents lblPEMarks As Label
+    Friend WithEvents lblEngNum As Label
+    Friend WithEvents lblMathNum As Label
+    Friend WithEvents lblPENum As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,7 +1,90 @@
 ﻿Public Class Form1
 
+    Dim txtName As String
+    Dim txtEnglish As Integer
+    Dim txtMaths As Integer
+    Dim txtPE As Integer
+    Dim x As Integer = 0 'number of people in the class accumilative
+    Dim y As String = "  people  "
+    Dim i As String = "  English:  "
+    Dim j As String = "  Maths:  "
+    Dim a As String = "  PE:  " 'defines a string variable so then it can be simply connected with an integer
+    Dim answer As Integer
+    Dim t As Integer = 0
+    Dim r As Integer = 0
+    Dim q As Integer = 0
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click 'enters the student data, with a single student's full results 
+
+        txtName = tbxName.Text 'connects the variables to the textboxes
+        txtEnglish = tbxEnglish.Text
+        txtMaths = tbxMaths.Text
+        txtPE = tbxPE.Text
+
+        If x >= 0 Then
+            x = x + 1
+            lblClass.Text = x & y
+        End If 'sees how many people there are in the year level and adds one every time the button is clicked
+
+        lblMarks.Text = txtName & i & txtEnglish & j & txtMaths & a & txtPE
+
+        answer = MsgBox("Add another student?", vbYesNo) 'asked if another student wants to be added
+
+        If answer = vbYes Then
+            tbxName.Clear() 'clears all textboxes
+            tbxEnglish.Clear()
+            tbxMaths.Clear()
+            tbxPE.Clear()
+        Else
+            Application.Exit()
+        End If 'closes application if they click no
+    End Sub
+
+    Private Sub btnNameEnter_Click(sender As Object, e As EventArgs) Handles btnNameEnter.Click
+
+        txtName = tbxName.Text 'connects the variables to the textboxes
+
+        lblAllNames.Text = txtName 'lists names individually 
+
+
+    End Sub
+
+    Private Sub btnEnglishEnter_Click(sender As Object, e As EventArgs) Handles btnEnglishEnter.Click
+
+        txtEnglish = tbxEnglish.Text 'connects the variables to the textboxes
+
+        lblEnglishMarks.Text = txtEnglish
+
+        If t >= 0 Then
+            t = t + 1
+            lblEngNum.Text = t & y 'total number of students in class
+        End If
+
+    End Sub
+
+    Private Sub btnMathsEnter_Click(sender As Object, e As EventArgs) Handles btnMathsEnter.Click
+
+        txtMaths = tbxMaths.Text 'connects the variables to the textboxes
+
+        lblMathsMarks.Text = txtMaths
+
+        If r >= 0 Then
+            r = r + 1
+            lblMathNum.Text = r & y 'total number of students in class
+        End If
+
+    End Sub
+
+    Private Sub btnPeEnter_Click(sender As Object, e As EventArgs) Handles btnPeEnter.Click
+
+        txtPE = tbxPE.Text 'connects the variables to the textboxes
+
+        lblPEMarks.Text = txtPE
+
+        If q >= 0 Then
+            q = q + 1
+            lblPENum.Text = q & y 'total number of students in class
+        End If
 
     End Sub
 End Class
